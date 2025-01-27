6d99f3ce4f9c995c31996cb069bb046413caa1dd
itc-s23020 <s23020@std.it-college.ac.jp>
2025-01-27 10:49:12 +0900

    firebase登録できた！googleloginができるようになりました!

---
+++ cocktail_search/src/lib/api.js
@@ -0,0 +1,11 @@
+module.exports = {
+    env: {
+    firebase_api_key: process.env.REACT_APP_API_KEY,
+    firebase_auth_domain: process.env.REACT_APP_AUTH_DOMAIN,
+    firebase_projectId: process.env.REACT_APP_PROJECT_ID,
+    firebase_storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    firebasemessagingSenderId: process.env.REACT_APP_MESSAGING_SenderId,
+    firebaseAppId: process.env.REACT_APP_APP_ID,
+    isFirebaseMessagingSenderId: process.env.REACT_APP_MESSAGING_SenderId,
+},
+}
